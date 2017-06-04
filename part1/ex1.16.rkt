e1622527f1e2187967533fb5249ed97b97c22117
Leira Hua <leirahua@gmail.com>
2017-06-04 14:38:17 +0800

    finished ex1.16

---
+++ part1/ex1.16.rkt
@@ -0,0 +1,34 @@
+#lang racket/base
+
+;; Exercise 1.16: Design a procedure that evolves an iterative exponentiation
+;; process that uses successive squaring and uses a logarithmic number of steps,
+;; as does fast-expt. (Hint: Using the observation that
+;; $(b^{n/2})^2=(b^2)^{n/2}$, keep, along with the exponent nn and the base b,
+;; an additional state variable a, and define the state transformation in such
+;; a way that the product $ab^n$ is unchanged from state to state. At the
+;; beginning of the process a is taken to be 1, and the answer is given by the
+;; value of a at the end of the process. In general, the technique of defining
+;; an invariant quantity that remains unchanged from state to state is a
+;; powerful way to think about the design of iterative algorithms.)
+
+(define (square x) (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-expt-iter a b n)
+  ; a: state, b: base, n: exponent
+  (cond ((= n 0) 
+         a)
+        ((even? n) 
+         (fast-expt-iter a (square b) (/ n 2)))
+        (else 
+         (fast-expt-iter (* a b) b (- n 1)))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (fast-expt-iter 1 2 0) 1)
+  (check-equal? (fast-expt-iter 1 3 1) 3)
+  (check-equal? (fast-expt-iter 1 2 4) 16)
+  (check-equal? (fast-expt-iter 1 2 5) 32))
+
