10b63570a5a5542f77b6a5b5771a6347a6938032
Leira Hua <leirahua@gmail.com>
2017-06-24 10:07:11 +0800

    finished ex2.29

---
+++ part2/ex2.29.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+;; Exercise 2.29 http://sarabander.github.io/sicp/html/2_002e2.xhtml#Exercise-2_002e29
+
+(define (make-mobile left right)
+  (list left right))
+(define (left-branch mobile)
+  (car mobile))
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (make-branch length structure)
+  (list length structure))
+(define (branch-length branch)
+  (car branch))
+(define (branch-structure branch)
+  (cadr branch))
+  
+(define (branch-weight branch)
+  (let ((s (branch-structure branch)))
+    (if (not (pair? s))
+        s
+        (total-weight s))))
+
+(define (total-weight mobile)
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+
+(define (mobile-balanced? mobile)
+  (define (branch-torque branch)
+    (* (branch-length branch)
+       (branch-weight branch)))
+  (define (branch-balanced? branch)
+    (let ((s (branch-structure branch)))
+      (if (not (pair? s))
+          #t
+          (mobile-balanced? s))))
+  (let ((lb (left-branch mobile))
+        (rb (right-branch mobile)))
+    (and (branch-balanced? lb)
+         (branch-balanced? rb)
+         (= (branch-torque lb)
+            (branch-torque rb)))))
+
+
+(module+ test
+  (require rackunit)
+  (define mb
+    (make-mobile 
+      (make-branch 2 
+                   (make-mobile
+                     (make-branch 2 1)
+                     (make-branch 1 2)))
+      (make-branch 2 3)))
+  (define mi
+    (make-mobile 
+      (make-branch 2 
+                   (make-mobile
+                     (make-branch 2 1)
+                     (make-branch 1 2)))
+      (make-branch 1 3)))
+  (check-equal? (total-weight mb) 6)
+  (check-equal? (total-weight mi) 6)
+  (check-true (mobile-balanced? mb))
+  (check-false (mobile-balanced? mi)))
+
